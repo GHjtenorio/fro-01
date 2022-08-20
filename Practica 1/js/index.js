636e2a6a588d6ce7f0c6e9e65663abcbd834616a
GHjtenorio <111647994+GHjtenorio@users.noreply.github.com>
2022-08-20 10:29:34 -0600

    Creacion carpeta practica 1
    Subiendo la carpeta de la practica # 1 para efetos de revision

---
+++ Practica 1/js/index.js
@@ -0,0 +1,32 @@
+
+
+// forma obsoleta
+// var nombre = 'jonathan';
+
+let nombre= 'jonathan';
+
+console.log(nombre);
+
+nombre='Andres';
+console.log(nombre);
+
+// creacion de una constante
+
+const PI = 3.14;
+
+console.log('El valor de PI es: ',PI);
+
+// cree dos variables:
+// num1 y asignele el valor de 7
+// num2 y asigne el valor 9
+// cree una vairable llamada reuslta1 que garde la suman de lso dos numeros
+// cree una variable llamda resulta2 que garde el resultade de la mutiplicaion de los 2 numeros
+
+let num1 = 7;
+let num2 = 9;
+
+let resultado1 = num1+num2;
+let resultado2 = num1*num2;
+
+console.log('El resultado de la suma de 7 y 9 es: ',resultado1);
+console.log('El resultado de la multiplicacion de 7 y 9 es: ',resultado2);
